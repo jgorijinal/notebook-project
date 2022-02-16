29ad8e9e20e53bca6cbd07e9980617c8461bad01
jgoriginal <932274204@qq.com>
2022-02-16 23:09:22 +0800

    init

---
+++ src/components/NotebookList.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="notebookList">
+  <h1>{{msg}}</h1>
+  <ul>
+    <router-link to="/note/1">1</router-link>
+    <router-link to="/note/2">2</router-link>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: "NotebookList",
+  data(){
+    return {
+      msg:'笔记本列表'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
