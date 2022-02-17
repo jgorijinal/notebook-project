fce3500bbe57cf6d742458dc2b484184740a2733
jgoriginal <932274204@qq.com>
2022-02-17 13:33:52 +0800

    install less

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -21,47 +21,49 @@
 <script>
 import Icon from './Icon.vue'
 import Avatar from './Avatar.vue'
+
 export default {
-  components: {Icon,Avatar}
+  components: {Icon, Avatar}
 }
 </script>
-<style scoped>
+<style lang="less" scoped>
 #sidebar {
   position: relative;
   width: 70px;
   text-align: center;
   background-color: #2c333c;
-}
 
-.icons {
-  margin-top: 15px;
-}
+  .icons {
+    margin-top: 15px;
 
-.icons a {
-  transition: all 0.3s;
-  padding: 12px 0;
-  display: block;
-}
 
-.icons a:hover {
-  background-color: #8a8a8c;
-}
+    a {
+      transition: all 0.3s;
+      padding: 12px 0;
+      display: block;
+    }
 
-.icons .router-link-active {
-  background-color: #5e6266;
-}
+    a:hover {
+      background-color: #8a8a8c;
+    }
 
-.logout {
-  position: absolute;
-  bottom: 20px;
-  width: 100%;
-  text-align: center;
-  cursor: pointer;
-}
+    .router-link-active {
+      background-color: #5e6266;
+    }
+  }
 
-.iconfont {
-  color: #fff;
-  width: 2em;
-  height: 2em;
+  .logout {
+    position: absolute;
+    bottom: 20px;
+    width: 100%;
+    text-align: center;
+    cursor: pointer;
+  }
+
+  .iconfont {
+    color: #fff;
+    width: 2em;
+    height: 2em;
+  }
 }
 </style>
