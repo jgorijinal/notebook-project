4f28181c07c2667187be5e224be41087f4d10a62
jgoriginal <932274204@qq.com>
2022-02-17 13:16:07 +0800

    添加SiIcon组件和Siderbar组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg class="iconfont" aria-hidden="true">
+    <use :xlink:href="`#icon-${name}`"></use>
+  </svg>
+</template>
+<script>
+import './iconfont'
+export default {
+  props:{
+    name:{
+      type:String,
+    }
+  }
+}
+</script>
+<style scoped>
+.iconfont {
+  width: 1.8em; height: 1.8em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="sidebar">
+    <div class="icons">
+      <router-link to="/note/1" title="笔记">
+        <Icon name="note" class="iconfont icon-note"></Icon>
+      </router-link>
+      <router-link to="/notebooks" title="笔记本">
+        <Icon name="notebook" class="iconfont icon-notebook"></Icon>
+      </router-link>
+      <router-link to="/trash/2" title="回收站">
+        <Icon name="trash" class="iconfont icon-trash"></Icon>
+      </router-link>
+    </div>
+    <div class="logout">
+      <Icon name="logout" class="iconfont icon-logout"></Icon>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from './Icon.vue'
+
+export default {
+  components: {Icon}
+}
+</script>
+<style scoped>
+#sidebar {
+  position: relative;
+  width: 70px;
+  text-align: center;
+  background-color: #2c333c;
+}
+
+.icons {
+  margin-top: 15px;
+}
+
+.icons a {
+  transition: all 0.3s;
+  padding: 12px 0;
+  display: block;
+}
+
+.icons a:hover {
+  background-color: #8a8a8c;
+}
+
+.icons .router-link-active {
+  background-color: #5e6266;
+}
+
+.logout {
+  position: absolute;
+  bottom: 20px;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+}
+
+.iconfont {
+  color: #fff;
+  width: 2em;
+  height: 2em;
+}
+</style>
